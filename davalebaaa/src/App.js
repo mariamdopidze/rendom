803a4352508b9bf68fda00f45b8b6c456b65379e
mariamdopidze <119893246+mariamdopidze@users.noreply.github.com>
2023-07-03 12:54:12 +0400

    added files

--- davalebaaa/src/App.js
+++ davalebaaa/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const students = ['მარიამ', 'სალომე', 'ანი', 'ნინი'];
+  const [groups, setGroups] = useState([]);
+
+  const distributeStudents = () => {
+    const shuffledStudents = [...students].sort(() => Math.random() - 0.5);
+    const dividedGroups = [[], [], [], []];
+
+    shuffledStudents.forEach((student, index) => {
+      const groupIndex = index % 4;
+      dividedGroups[groupIndex].push(student);
+    });
+
+
+    setGroups(dividedGroups);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={distributeStudents}>განაწილების ქუდი</button>
+      
+      {groups.map((group, index) => (
+        <div key={index}>
+          <h3>გრიფინდორი {index + 1}</h3>
+          
+          <ul>
+            {group.map((student, studentIndex) => (
+              <li key={studentIndex}>{student}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+      
     </div>
+    
   );
-}
+};
 
 export default App;
