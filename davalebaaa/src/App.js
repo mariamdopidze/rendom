458e0092e0f79c1eb58abcf67d5b702af9555d20
mariamdopidze <119893246+mariamdopidze@users.noreply.github.com>
2023-07-04 21:19:05 +0400

    added files

--- davalebaaa/src/App.js
+++ davalebaaa/src/App.js
@@ -1,41 +1,73 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 
-const App = () => {
-  const students = ['მარიამ', 'სალომე', 'ანი', 'ნინი'];
-  const [groups, setGroups] = useState([]);
+function App(){
+  const students =['ნინი', 'სალომე', 'ანი','მარიამი'];
 
-  const distributeStudents = () => {
-    const shuffledStudents = [...students].sort(() => Math.random() - 0.5);
-    const dividedGroups = [[], [], [], []];
+  const [gryffindor, setGryffindor]= useState([]);
+  const [reivenqlo, setReivenqlo]= useState([]);
+  const [slizerin, setSlizerin]= useState([]);
+  const [gryf, setGryf]= useState([]);
 
-    shuffledStudents.forEach((student, index) => {
-      const groupIndex = index % 4;
-      dividedGroups[groupIndex].push(student);
-    });
+const handleClick =()=>{
+  for(let i= 0; i < students.length; i++){
+    const num =Math.floor(Math.random()*3)+1;
+
+if(num===1)setGryffindor((prev)=>[...prev, students[i]]);
+if(num===2)setReivenqlo((prev)=>[...prev, students[i]]);
+if(num===3)setSlizerin((prev)=>[...prev, students[i]]);
+if(num===4)setGryf((prev)=>[...prev, students[i]]);
+
+  }
+};
 
 
-    setGroups(dividedGroups);
-  };
+return(
+  <>
+  <div>
+<ul className='list-group'>
+{students.map((student)=>(
+  <li className='list-group-item'>{student}</li>
+))}
 
-  return (
-    <div>
-      <button onClick={distributeStudents}>განაწილების ქუდი</button>
-      
-      {groups.map((group, index) => (
-        <div key={index}>
-          <h3>გრიფინდორი {index + 1}</h3>
-          
-          <ul>
-            {group.map((student, studentIndex) => (
-              <li key={studentIndex}>{student}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
-      
-    </div>
-    
-  );
+</ul>
+<div className='row'>
+  <div className='col-3'>
+<h3>გრიფინდორი</h3>
+{gryffindor.map((gr)=>(
+  <li>{gr}</li>
+))}
+  </div>
+
+  <div className='col-3'>
+<h3>რეივენქლო</h3>
+{reivenqlo.map((gr)=>(
+  <li>{gr}</li>
+))}
+  </div>
+
+  <div className='col-3'>
+<h3>სლიზერინი</h3>
+{slizerin.map((gr)=>(
+  <li>{gr}</li>
+))}
+  </div>
+
+  <div className='col-3'>
+<h3>ჰაფელჰაფი</h3>
+{gryf.map((gr)=>(
+  <li>{gr}</li>
+))}
+  </div>
+
+</div>
+<button className='btn btn-primary' onClick={handleClick}>გამანაწილებელი ქუდი</button>
+ 
+  </div>
+  </>
+
+  
+)
+
 };
 
 export default App;
